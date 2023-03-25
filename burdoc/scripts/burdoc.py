813e4c81a81f95c0dc01236f00c6548de5155325
Joe <joe.ennis0@gmail.com>
2023-03-25 00:41:48 +0000

    Add basic CLI script

--- burdoc/scripts/burdoc.py
+++ burdoc/scripts/burdoc.py
@@ -5,29 +5,30 @@ import logging
 
 from ..burdoc_parser import BurdocParser
 
-argparser = argparse.ArgumentParser()
+def run():
+    argparser = argparse.ArgumentParser()
 
-argparser.add_argument("file", type=str, help="Path to the PDF file you want to prase")
-argparser.add_argument('outfile', type=str, help="Path to file to write output to", nargs="?")
+    argparser.add_argument("file", type=str, help="Path to the PDF file you want to prase")
+    argparser.add_argument('outfile', type=str, help="Path to file to write output to", nargs="?")
 
-args = argparser.parse_args()
+    args = argparser.parse_args()
 
-parser = BurdocParser(log_level=logging.INFO)
+    parser = BurdocParser(log_level=logging.INFO)
 
-if os.path.exists(args.file):
-    print(f"Parsing {args.file}")
-    out = parser.read(args.file)
+    if os.path.exists(args.file):
+        print(f"Parsing {args.file}")
+        out = parser.read(args.file)
 
-    if not args.outfile:
-        if ".pdf" in args.file:
-            outfile = args.file.replace(".pdf", ".json")
+        if not args.outfile:
+            if ".pdf" in args.file:
+                outfile = args.file.replace(".pdf", ".json")
+            else:
+                outfile = str(args.file) + ".json"
         else:
-            outfile = str(args.file) + ".json"
-    else:
-        outfile = args.outfile
+            outfile = args.outfile
 
-    print(f"Writing output to {outfile}")
-    with open(outfile, 'w') as f:
-        json.dump(out, f)
-else:
-    raise FileNotFoundError(args.file)
+        print(f"Writing output to {outfile}")
+        with open(outfile, 'w') as f:
+            json.dump(out, f)
+    else:
+        raise FileNotFoundError(args.file)
