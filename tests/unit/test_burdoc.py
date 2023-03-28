8e1dab60079fe657b88ff6ab17162733b40d9fce
Joe <joe.ennis0@gmail.com>
2023-03-28 18:06:04 +0100

    Don't try to import MLTableProcessor in tests when torch isn't installed

--- tests/unit/test_burdoc.py
+++ tests/unit/test_burdoc.py
@@ -1,6 +1,11 @@
 import burdoc.burdoc_parser
 from burdoc.burdoc_parser import BurdocParser
-from burdoc.processors.table_processors.ml_table_processor import MLTableProcessor
+
+try:
+    from burdoc.processors.table_processors.ml_table_processor import MLTableProcessor
+    _TEST_HAS_TRANSFORMERS=True
+except ImportError:
+    _TEST_HAS_TRANSFORMERS=False
 
 
 class TestBurdocParser():
@@ -9,9 +14,10 @@ class TestBurdocParser():
         parser = BurdocParser()
     
     def test_init_with_transformers(self):
-        burdoc.burdoc_parser._HAS_TRANSFORMERS = True
-        parser = BurdocParser()
-        assert parser.processors[1][0] == MLTableProcessor
+        if _TEST_HAS_TRANSFORMERS:
+            burdoc.burdoc_parser._HAS_TRANSFORMERS = True
+            parser = BurdocParser()
+            assert parser.processors[1][0] == MLTableProcessor
         
     def test_init_no_transformers(self):
         burdoc.burdoc_parser._HAS_TRANSFORMERS = False
